4cdee7637cd570f9adc1537f883b83c67faae52e
Aleksey Krasilov <alekseykrasilov_2015@mail.ru>
2022-11-22 13:10:50 +0300

    Commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,27 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.util.Util;
+
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+
+        Util.getConnectionToDB();
+        UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
+
+        userDao.createUsersTable();
+
+        userDao.saveUser("name1", "LastName1", (byte) 22);
+        userDao.saveUser("name2", "LastName2", (byte) 32);
+        userDao.saveUser("name3", "LastName3", (byte) 21);
+        userDao.saveUser("name4", "LastName4", (byte) 18);
+
+        userDao.removeUserById(1);
+        System.out.println(userDao.getAllUsers());
+        userDao.cleanUsersTable();
+        userDao.dropUsersTable();
+
     }
 }
